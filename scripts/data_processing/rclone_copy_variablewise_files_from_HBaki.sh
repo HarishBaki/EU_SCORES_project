4b9776908366f3380a6ec32cd6f82dfccf95da29
HarishBaki <h.baki@tudelft.nl>
2024-03-15 10:11:46 +0100

    modified:   scripts/data_processing/rclone_copy_variablewise_files_from_HBaki.sh

--- scripts/data_processing/rclone_copy_variablewise_files_from_HBaki.sh
+++ scripts/data_processing/rclone_copy_variablewise_files_from_HBaki.sh
@@ -1,3 +1,5 @@
+# Always, run a rclone with --dry-run first. Once you are confident enough, then remove this flag
+root_dir='/media/harish/SSD_4TB/EU_SCORES_project'
 run='New_runs'
 cases=('Portugal_coast' 'Netherlands_coast' 'Ireland_coast')
 
@@ -8,7 +10,7 @@ for exclude_file in "${exclude_files[@]}"; do
 done
 
 for case in "${cases[@]}"; do
-	rclone copy --progress --transfers 12 HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files $run/$case/Postprocessed/variablewise_files $exclude_args
+	rclone copy --progress --dry-run --transfers 12 HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files $root_dir/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files $exclude_args
 done
 
 run='Validation_runs'
@@ -21,5 +23,5 @@ for exclude_file in "${exclude_files[@]}"; do
 done
 
 for case in "${cases[@]}"; do
-	rclone copy --progress --transfers 12 HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files $run/$case/Postprocessed/variablewise_files $exclude_args
+	rclone copy --progress --dry-run --transfers 12 HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files $root_dir/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files $exclude_args
 done
