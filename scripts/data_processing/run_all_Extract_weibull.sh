839e52b7ef341895e512fc57e76d91fba7d3a061
HarishBaki <h.baki@tudelft.nl>
2024-03-18 11:38:48 +0100

    Add script for extracting Weibull wind data

---
+++ scripts/data_processing/run_all_Extract_weibull.sh
@@ -0,0 +1,17 @@
+#conda activate wrf-python-stable
+root_dir='/media/harish/SSD_4TB/EU_SCORES_project'
+run='New_runs'
+case='Germany_coast'
+levels=(10) # 80 100 120 150)
+I=(0 1 2)
+J=(0 1 2)
+for level in "${levels[@]}"; do
+	mkdir -p $root_dir/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/variablewise_files/weibull_$level
+	for i in "${I[@]}"; do
+		for j in "${J[@]}"; do
+			python $root_dir/scripts/data_processing/Extract_wind_weibull.py "$run" "$case" "$level" "$i" "$j" &
+			echo "Processing for level: $level, i: $i, j: $j"
+		done
+	done
+done
+echo "Script execution complete."
