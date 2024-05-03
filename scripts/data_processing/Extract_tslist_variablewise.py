405284ae9dc9b2e44dca209ab49b72dd11421c3c
HarishBaki <h.baki@tudelft.nl>
2024-05-03 15:18:59 +0200

    Refactor data processing scripts to improve file organization and readability

--- scripts/data_processing/Extract_tslist_variablewise.py
+++ scripts/data_processing/Extract_tslist_variablewise.py
@@ -14,6 +14,15 @@ from datetime import datetime, timedelta
 import numpy as np
 import wrf
 
+root_dir = '/media/harish/SSD_4TB/EU_SCORES_project'
+scripts_dir = f'{root_dir}/scripts'
+sys.path.append(scripts_dir)
+
+run = sys.argv[1]
+case = sys.argv[2]
+variable = sys.argv[3]
+location = sys.argv[4]
+
 import pandas as pd
 
 if __name__ == "__main__":
@@ -23,11 +32,15 @@ if __name__ == "__main__":
     # Connect to the cluster
     client = dask.distributed.Client(cluster)
     print(client)
-    for variable in ['TS','UU','VV']:
-        start_time = time.time()
-        df = dd.read_csv(f'intermediate_files/POR2_{variable}*.csv').set_index('Unnamed: 0')
-        dd.to_csv(df,f'variablewise_files/{variable}.csv',single_file=True,compute=True)
-        end_time = time.time()
-        print(f'Elapsed time {end_time-start_time}s for {variable}')
+
+    run_dir=f'{root_dir}/WRFV4.4/EU_SCORES/{run}/{case}/Postprocessed'
+    target_file = f'{run_dir}/variablewise_files/{location}_{variable}.csv'
+
+    start_time = time.time()
+    df = dd.read_csv(f'{run_dir}/intermediate_files/{location}_{variable}*.csv').set_index('Unnamed: 0')
+    dd.to_csv(df,target_file,single_file=True,compute=True)
+    end_time = time.time()
+    print(f'Elapsed time {end_time-start_time}s for {variable}')
+
     client.close()
     cluster.close()
--- scripts/data_processing/rclone_copy_intermediate_files.sh
+++ scripts/data_processing/rclone_copy_intermediate_files.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+
+root_dir='/media/harish/SSD_4TB/EU_SCORES_project'
+runs=('New_runs' 'Validation_runs')
+cases=('Netherlands_coast' 'Germany_coast' 'Portugal_coast' 'Ireland_coast')
+
+run='New_runs'
 case="Netherlands_coast"
 
+: '
+# the following script copies the intermediate files of WRF runs from the server to the local machine
 files=( )
 for i in {1..2265}; do
 	files[${#files[*]}]="WRF_$i.nc"
 done
 printf "%s\n" "${files[@]}" > files.txt
 
-rclone copy --progress --transfers 12 --files-from files.txt HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/WRFV4.4/EU_SCORES/New_runs/$case/Postprocessed/intermediate_files intermediate_files/.
+rclone copy --progress --transfers 12 --files-from files.txt HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/EU_SCORES_project/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/intermediate_files $root_dir/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/intermediate_files/.
 
-: '
+# the following script copies the intermediate files of tslist from the server to the local machine
+'
+
 files=( )
-for i in {1..2265}; do
-	files[${#files[*]}]="POR2_TS_$i.csv"
-	files[${#files[*]}]="POR2_UU_$i.csv"
-	files[${#files[*]}]="POR2_VV_$i.csv"
+for i in {1900..2046}; do
+	#files[${#files[*]}]="LOT1_TS_$i.csv"
+	files[${#files[*]}]="LOT2_UU_$i.csv"
+	files[${#files[*]}]="LOT2_VV_$i.csv"
 done
 printf "%s\n" "${files[@]}" > files.txt
 
-rclone copy --progress --transfers 12 --files-from files.txt HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/WRFV4.4/EU_SCORES/New_runs/$case/Postprocessed/intermediate_files intermediate_files/.
-'
+rclone copy --progress --transfers 12 --files-from files.txt HBaki_sftp:/tudelft.net/staff-umbrella/HBaki/EU_SCORES_project/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/intermediate_files $root_dir/WRFV4.4/EU_SCORES/$run/$case/Postprocessed/intermediate_files/.
+
