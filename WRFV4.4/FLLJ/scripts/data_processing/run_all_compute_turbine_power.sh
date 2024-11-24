13e1534de45c3ef02bb648f73b7b252a15100c48
HarishBaki <h.baki@tudelft.nl>
2024-11-24 20:11:54 +0100

    Update run_all_compute_turbine_power.sh to include additional runs and improve script structure. Basically, run+7 are removed to directly include runs from 1 to 17. Case 3 were included.

--- WRFV4.4/FLLJ/scripts/data_processing/run_all_compute_turbine_power.sh
+++ WRFV4.4/FLLJ/scripts/data_processing/run_all_compute_turbine_power.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 # This script runs all the scripts needed to compute the turbine power
 # for the WRF simulations.
-: '
-for case in 1 2
-do
-    for run in 8; do
-        run_dir="WRF_run_$run"
-        python extract_POWER.py $case $run $run_dir
-        echo $case $run $run_dir
-    done
 
-    for run in 2 5
-    do
-        run_dir="WRF_run_$run"
-        python extract_POWER.py $case $run $run_dir
-        echo $case $run $run_dir
-        run_dir="WRF_run_"$((run + 7))
-        python extract_POWER.py $case $run $run_dir
-        echo $case $run $run_dir
-    done
 
-done
-
-for case in 3
-do
-    for run in 5; do
+# This loop extracts the POWER data from the MYNN simulations, which inherently compute the power from turbine models. 
+for case in 1 2; do  # this loop is for the cases 1 and 2
+    # This is loop for simulations with 3 hr CERRA data and 1 hr CERRA data
+    for run in 1 2 5 8 9 12 16 17; do  # this loop is for the runs 1, 2, 5, 8, 9, 12, 16, 17
         run_dir="WRF_run_$run"
         python extract_POWER.py $case $run $run_dir
         echo $case $run $run_dir
     done
 done
-'
 
+for case in 3; do   # this loop is for the case 3
+    for run in 2 5; do
+        run_dir="WRF_run_$run"
+        python extract_POWER.py $case $run $run_dir
+        echo $case $run $run_dir
+    done
+done
+
+# This loop computes the POWER data from the non-MYNN simulations. 
 # Semaphore to limit concurrent processes
 MAX_CONCURRENT_JOBS=48
 # Current number of running jobs
@@ -44,50 +34,10 @@ function wait_for_completion {
     done
 }
 
-
 # number_of_turbines are 182
-for case in 1 2
-do
-    for run in 15 #3 4 6 7
-    do
-        run_dir="WRF_run_$run"
-        for j in $(seq 0 181)
-        do
-            # Wait until the number of running jobs is less than the maximum allowed
-            wait_for_completion
-            python compute_turbine_power.py $case $run $run_dir $j "False" &
-            CURRENT_JOBS=$(jobs | wc -l)
-            echo $case $run $j
-        done
-        wait
-        # Combine the weibull parameters along individual south_north direciton and all west_east direction.
-        python compute_turbine_power.py $case $run $run_dir 0 "True" 
-        echo $case $run
-        wait
-        : '
-        run_dir="WRF_run_"$((run + 7))
-        for j in $(seq 0 181)
-        do
-            # Wait until the number of running jobs is less than the maximum allowed
-            wait_for_completion
-            python compute_turbine_power.py $case $run $run_dir $j "False" &
-            CURRENT_JOBS=$(jobs | wc -l)
-            echo $case $run $j
-        done
-        wait
-        # Combine the weibull parameters along individual south_north direciton and all west_east direction.
-        python compute_turbine_power.py $case $run $run_dir 0 "True" 
-        echo $case $run
-        wait
-        '
-    done
-    wait
-done
-: '
-for case in 3
-do
-    for run in 7
-    do
+for case in 1 2; do 
+    # This is loop for simulations with 3 hr CERRA data and 1 hr CERRA data
+    for run in 3 4 6 7 10 11 13 14 15; do   # this loop is for the runs 3, 4, 6, 7, 15
         run_dir="WRF_run_$run"
         for j in $(seq 0 181)
         do
@@ -105,7 +55,28 @@ do
     done
     wait
 done
-'
+wait
+
+for case in 3; do   
+    # This is loop for simulations with 3 hr CERRA data
+    for run in 3 4; do  # this loop is for the runs 3, 4, 
+        run_dir="WRF_run_$run"
+        for j in $(seq 0 181)
+        do
+            # Wait until the number of running jobs is less than the maximum allowed
+            wait_for_completion
+            python compute_turbine_power.py $case $run $run_dir $j "False" &
+            CURRENT_JOBS=$(jobs | wc -l)
+            echo $case $run $j
+        done
+        wait
+        # Combine the weibull parameters along individual south_north direciton and all west_east direction.
+        python compute_turbine_power.py $case $run $run_dir 0 "True" 
+        echo $case $run
+        wait
+    done
+    wait
+done
 wait
 
 echo "All done"
